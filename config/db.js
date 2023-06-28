9cd1868de6e55979c01258f8e7bf97db1dc8fbf5
nischal <shakyanischal1111@gmail.com>
2023-06-28 14:56:29 +0545

    job-portal

--- config/db.js
+++ config/db.js
@@ -1,4 +1,9 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/job-portal')
-  .then(() => console.log('DataBaseConnected!'));
+// mongoose.connect('mongodb://127.0.0.1:27017/job-portal')
+//   .then(() => console.log('DataBaseConnected!'));
+
+const DB = 'mongodb+srv://shakyanischal:shakya@cluster0.kfqbaqo.mongodb.net/jobportal?retryWrites=true&w=majority'
+mongoose.connect(DB).then(()=>{
+  console.log(`connection successful`);
+}).catch((err)=> console.log(`connection error`));
