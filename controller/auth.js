45e6f3518fe2aeb665720b2f76f44bfb0919bf58
nischal <shakyanischal1111@gmail.com>
2023-09-16 13:11:11 +0545

    login

--- controller/auth.js
+++ controller/auth.js
@@ -113,7 +113,7 @@ const login = async (req, res, next) => {
 
               res.send({
                   msg: "login successful",
-                //   user,
+                  user,
                   token
               })
               return;
