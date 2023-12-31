c80f03d4ec85e883f70a13e8d6c0d8f1e6576eb6
nischal <shakyanischal1111@gmail.com>
2024-01-01 00:56:46 +0545

    userProfile

--- routes/profile_routes.js
+++ routes/profile_routes.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const {createProfile} = require('../controller/profile_controller')
 const{ authenticationCheck, isSeeker, isCompany} = require('../middleware/authenticationCheck')
 
-router.post("/", createProfile) 
+router.post("/",authenticationCheck, createProfile) 
 
 
 module.exports = router
