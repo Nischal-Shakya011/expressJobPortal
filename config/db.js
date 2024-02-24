248f00e119cba3583ca28878f9484d2c79bad9fa
nischal <shakyanischal1111@gmail.com>
2024-02-24 12:55:36 +0545

    database

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 // mongoose.connect('mongodb://127.0.0.1:27017/job-portal')
 //   .then(() => console.log('DataBaseConnected!'));
 
-const DB = 'mongodb+srv://shakyanischal:shakya@cluster0.kfqbaqo.mongodb.net/jobportal?retryWrites=true&w=majority'
+const DB = 'mongodb+srv://shakyanischal:j7yvICvxCeXTT78O@cluster0.kfqbaqo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 mongoose.connect(DB).then(()=>{
   console.log(`DB connection successful`);
-}).catch((err)=> console.log(`DB connection error`));
+}).catch((err)=> console.log(`DB connection error`, err));
