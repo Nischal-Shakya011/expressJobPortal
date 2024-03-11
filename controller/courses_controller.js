25a17a630abc9d8b918c36749264731affdfa768
nischal <shakyanischal1111@gmail.com>
2024-03-12 01:21:36 +0545

    courses

--- controller/courses_controller.js
+++ controller/courses_controller.js
@@ -11,10 +11,43 @@ const createCourses = async (req, res, next) => {
         next(err)
     }
 }
+const getCourses = async (req, res, next) => {
+    try {
+  
 
+    let training = await Course.find();
+        res.send(training)
+    } 
+    
+    catch (err) {
+        next(err)
+    }
+}
+const fetchSingleCourse = async (req, res, next)=>{
 
+    try{
+   
+        let job = await Course.findById(req.params.id)
+        
+        if(job){
+            res.send(job)
+        }
+        else
+        {
+            res.status(404).send({
+                msg:"Resource not found"
+            })
+        }
+
+    }
+    catch(err){
+      next(err)
+    }
+}
 
 
 module.exports ={
-    createCourses
+    createCourses,
+    getCourses,
+    fetchSingleCourse
 }
--- routes/courses_routes.js
+++ routes/courses_routes.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router()
 
-const {createCourses} = require('../controller/courses_controller')
+const {createCourses, getCourses, fetchSingleCourse} = require('../controller/courses_controller')
 const{ authenticationCheck, isSeeker, isCompany} = require('../middleware/authenticationCheck')
 
 router.post("/", authenticationCheck, createCourses) 
+router.get("/", getCourses) 
+router.get("/:id", fetchSingleCourse) 
 
 module.exports = router
