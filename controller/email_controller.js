5e345af93bb5fb581a247fa96d83a84ba07c50c5
nischal <shakyanischal1111@gmail.com>
2024-03-12 22:09:12 +0545

    email

--- controller/email_controller.js
+++ controller/email_controller.js
@@ -22,7 +22,7 @@ const transporter = nodemailer.createTransport({
         to: email,
         subject: subject,
         // html : `<h1>${message}</h1>`
-        html : `<p>This is Link${message}</p>`
+        html : `<p>${message}</p>`
     };
     transporter.sendMail(mailOptions, function(error, info){
         if(error){
@@ -36,21 +36,21 @@ const transporter = nodemailer.createTransport({
 
   const sendRejectEmail = expressAsyncHandler(async(req, res)=>{
     const { email } = req.body;
-    // console.log(email, subject, message);
+    console.log(email, subject, message);
     var mailOptions = {
         from: process.env.SMTP_MAIL,
         to: email,
-        subject: "Rejected",
-        // html : `<h1>${message}</h1>`
-        html : `<p>Dear,<br/>
-        We appreciate the time and effort you invested in applying for this job at our Company.
-        After careful consideration, we regret to inform you that we have chosen to move forward with 
-        other candidates whose qualifications more closely align with our current needs.
-        We encourage you to continue exploring opportunities that match your skills and career goals.
-        And we want to encourage you to monitor our courses for growing your skills according to your interest.
-        <br/>https://react-job-portal-one.vercel.app/jobStatus
-        <br/>Thank you once again for your interest in our Company. We wish you continued success in your career endeavors.
-        </p>`
+        subject: subject,
+        html : `<p>${message}</p>`
+        // html : `<p>Dear,<br/>
+        // We appreciate the time and effort you invested in applying for this job at our Company.
+        // After careful consideration, we regret to inform you that we have chosen to move forward with 
+        // other candidates whose qualifications more closely align with our current needs.
+        // We encourage you to continue exploring opportunities that match your skills and career goals.
+        // And we want to encourage you to monitor our courses for growing your skills according to your interest.
+        // <br/>https://react-job-portal-one.vercel.app/jobStatus
+        // <br/>Thank you once again for your interest in our Company. We wish you continued success in your career endeavors.
+        // </p>`
     };
     transporter.sendMail(mailOptions, function(error, info){
         if(error){
